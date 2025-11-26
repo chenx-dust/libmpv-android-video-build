eae6403b0d741c54368299ea07d58e9b0f8df7a1
Page Chen <pagechen04@gmail.com>
2025-11-27 04:23:43 +0800

    roll back

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -9,13 +9,13 @@ mkdir -p deps && cd deps
 git config --global advice.detachedHead false
 
 # mbedtls
-[ ! -d mbedtls ] && git clone --depth 1 --branch v$v_mbedtls --recurse-submodules https://github.com/Mbed-TLS/mbedtls.git mbedtls
+[ ! -d mbedtls ] && git clone --depth 1 --branch v$v_mbedtls --recurse-submodules --shallow-submodules https://github.com/Mbed-TLS/mbedtls.git mbedtls
 
 # dav1d
 [ ! -d dav1d ] && git clone --depth 1 --branch $v_dav1d https://code.videolan.org/videolan/dav1d.git dav1d
 
 # libxml2
-[ ! -d libxml2 ] && git clone --depth 1 --branch v$v_libxml2 --recursive https://gitlab.gnome.org/GNOME/libxml2.git libxml2
+[ ! -d libxml2 ] && git clone --depth 1 --branch v$v_libxml2 --recursive --shallow-submodules https://gitlab.gnome.org/GNOME/libxml2.git libxml2
 
 # libogg
 [ ! -d libogg ] && $WGET https://github.com/xiph/ogg/releases/download/v${v_libogg}/libogg-${v_libogg}.tar.gz && tar -xf libogg-${v_libogg}.tar.gz && mv libogg-${v_libogg} libogg && rm libogg-${v_libogg}.tar.gz
@@ -55,7 +55,7 @@ see <ndk>/sources/third_party/shaderc
 HEREDOC
 
 # libplacebo
-[ ! -d libplacebo ] && git clone --depth 1 --branch v$v_libplacebo --recurse-submodules https://code.videolan.org/videolan/libplacebo.git libplacebo
+[ ! -d libplacebo ] && git clone --depth 1 --branch v$v_libplacebo --recurse-submodules --shallow-submodules https://code.videolan.org/videolan/libplacebo.git libplacebo
 
 # vulkan
 [ ! -d vulkan ] && git clone --depth 1 --branch v$v_vulkan https://github.com/KhronosGroup/Vulkan-Headers vulkan
